6b7cfef3e06651e20bc24b60d505da0cc20e6001
Vardan Galstyan <vardan.galstjan@gmail.com>
2024-08-17 23:38:44 +0200

    chore: updates vercel json

--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -1,54 +1,54 @@
-import ReactDOMServer from 'react-dom/server'
-import { StaticRouter } from 'react-router-dom'
-import { App } from './App'
-import router from './router'
-import { SSRProvider } from './context'
+import ReactDOMServer from "react-dom/server";
+import { StaticRouter } from "react-router-dom";
+import { App } from "./App";
+import router from "./router";
+import { SSRProvider } from "./context";
 
-async function loadData (url, context) {
-    const routes = router.match(url.replace(/\?.*$/, ''))
-    const paths = []
-    const promises = routes.map(e => {
-        paths.push(e.route.path)
-        return e.route.component.loadData
-            ? e.route.component.loadData({ ...context, params: e.match.params })
-            : null
-    })
+async function loadData(url, context) {
+  const routes = router.match(url.replace(/\?.*$/, ""));
+  const paths = [];
+  const promises = routes.map((e) => {
+    paths.push(e.route.path);
+    return e.route.component.loadData
+      ? e.route.component.loadData({ ...context, params: e.match.params })
+      : null;
+  });
 
-    const arr = await Promise.all(promises)
-    const dict = {}
+  const arr = await Promise.all(promises);
+  const dict = {};
 
-    for (const i in arr) {
-        dict[paths[i]] = {
-            url: url,
-            data: arr[i]
-        }
-    }
+  for (const i in arr) {
+    dict[paths[i]] = {
+      url: url,
+      data: arr[i],
+    };
+  }
 
-    return dict
+  return dict;
 }
 
-export async function render(url, context) {
-    let data = null
+export default async function render(url, context) {
+  let data = null;
 
-    try {
-        data = await loadData(url, context)
-    } catch (err) {
-        data = { $ssrErrorMsg: import.meta.env.DEV ? err.stack : err.message }
+  try {
+    data = await loadData(url, context);
+  } catch (err) {
+    data = { $ssrErrorMsg: import.meta.env.DEV ? err.stack : err.message };
+  }
+
+  for (const i in data) {
+    if (data[i].data && data[i].data.redirect) {
+      return { redirect: data[i].data.redirect };
     }
+  }
 
-    for (const i in data) {
-        if (data[i].data && data[i].data.redirect) {
-            return { redirect: data[i].data.redirect }
-        }
-    }
+  const html = ReactDOMServer.renderToString(
+    <SSRProvider value={data}>
+      <StaticRouter location={url} context={context}>
+        <App></App>
+      </StaticRouter>
+    </SSRProvider>
+  );
 
-    const html = ReactDOMServer.renderToString(
-        <SSRProvider value={data}>
-            <StaticRouter location={url} context={context}>
-                <App></App>
-            </StaticRouter>
-        </SSRProvider>
-    )
-
-    return { appHtml: html, propsData: data }
+  return { appHtml: html, propsData: data };
 }
